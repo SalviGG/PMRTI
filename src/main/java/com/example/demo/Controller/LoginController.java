392d2948a825722406a8dcaea8db1580ac7b1c8d
sgutierrez <sgutierrez@VLINFSGUTIERREZ.subpescaintra.cl>
2018-05-16 18:46:49 -0300

    dashboard installation

---
+++ src/main/java/com/example/demo/Controller/LoginController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo.Controller;
+
+import com.example.demo.model.entity.TipoEquipo;
+import com.example.demo.model.service.TipoEquipoService;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.support.SessionStatus;
+
+/**
+ *
+ * @author sgutierrez
+ */
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+    
+    @GetMapping("page-login")
+    public String login(){
+        return "login/page-login";
+    }
+    
+}
